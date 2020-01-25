16bd75aa70f95817cb8ab2fccec0007767c0af80
jayohanna <33985435+jayohanna@users.noreply.github.com>
2020-01-25 18:45:45 -0500

    added search bar

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { DatePicker, message } from 'antd';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Button, Layout, Menu, Breadcrumb } from 'antd';
+import { Button, Input, Layout, Menu, Breadcrumb } from 'antd';
 const { Header, Content, Footer } = Layout;
+const { Search } = Input;
 
 class App extends React.Component {
   state = {
@@ -30,27 +31,36 @@ class App extends React.Component {
       .catch(console.log);
   };
 
+  search = async value => {
+    if (value.length > 0) {
+      fetch("https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs?query=" + value, {
+        method: "get",
+        mode: "no-cors",
+        headers: {
+            'Authorization': '2e8d7cd2f48c9a0ab93d2c45a73013de'
+        }
+    })
+      .then(res => res.json())
+      .catch(console.log);
+    }
+  };
+
   render() {
     const { date } = this.state;
     return (
         <Layout className="layout">
-        <Header>
+        <Header style={{ zIndex: 1, padding: '10px', height: 80, textAlign: 'center'}}>
           <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
+          <Search
+              placeholder="input search text"
+              style={{width: 600}}
+              onSearch={value => this.search(value)}
+            />
         </Header>
-        <Content style={{ padding: '0 50px' }}>
+        <Content style={{ padding: '0 50px', marginTop: '200px' }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
           </Breadcrumb>
-            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            <div style={{ background: '#fff', padding: 24, minHeight: 1000 }}>
                 <Button type="primary" style={{ marginLeft: 8 }} onClick={this.handleChange}>
                     Primary Button
                 </Button>
