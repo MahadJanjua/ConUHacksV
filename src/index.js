7e353a0551db98020867ec2c471dd13b19aee07f
jayohanna <33985435+jayohanna@users.noreply.github.com>
2020-01-25 22:50:57 -0500

    added play and pause button

--- src/index.js
+++ src/index.js
@@ -13,22 +13,58 @@ class App extends React.Component {
 
 // https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs/56356209
   handleChange = async () => {
-    fetch('http://localhost:8080/play?url="https://cdn.apps.playnetwork.com/master/65e2c70ba416c00cf710bc3dc698c38108c19ccf778aabe36098d8d1d225fb5a.ogg?Signature=BxTi7I9Xevq3BnS74XemBXUMb-jyCl8Qh7lGSaUdmR6WOINPDyIiRi196tSI7XVHU~6UPwxgONAYjUbkclNb1bRcqDgLVw~ZV8su7EjMKjaXaukrGESfTC5c-JZd29Ny4ZXmVcahJXc-E-pGjr0SPhpSt0r5~Em7WHHJs3zReHQWa5LJACRZ9u5DVT4PeMLTUcH0rqOiLj3rGAPiqOA8~B7JsAnLQcatMnQFu9FqYxznWtOcFacLskzPVOSC4jzpaI~PEu6ifGXphG9VZ4SxUuLTgVJxksyzy1pquXfFwcSnUPaVyaZ5HVudT2RCVHsxBn1uP-vXCyX3EVcW3jseHQ__&Key-Pair-Id=APKAJ4GOPJEICF5TREYA&Expires=1579984449"', {
-        method: "post",
-        mode: "no-cors",
-        headers: {
-            'Authorization': '2e8d7cd2f48c9a0ab93d2c45a73013de'
-        }
-    })
-    // fetch("https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs/56356209", {
-    //     method: "get",
+    // fetch('http://localhost:8080/play?url="https://cdn.apps.playnetwork.com/master/65e2c70ba416c00cf710bc3dc698c38108c19ccf778aabe36098d8d1d225fb5a.ogg?Signature=BxTi7I9Xevq3BnS74XemBXUMb-jyCl8Qh7lGSaUdmR6WOINPDyIiRi196tSI7XVHU~6UPwxgONAYjUbkclNb1bRcqDgLVw~ZV8su7EjMKjaXaukrGESfTC5c-JZd29Ny4ZXmVcahJXc-E-pGjr0SPhpSt0r5~Em7WHHJs3zReHQWa5LJACRZ9u5DVT4PeMLTUcH0rqOiLj3rGAPiqOA8~B7JsAnLQcatMnQFu9FqYxznWtOcFacLskzPVOSC4jzpaI~PEu6ifGXphG9VZ4SxUuLTgVJxksyzy1pquXfFwcSnUPaVyaZ5HVudT2RCVHsxBn1uP-vXCyX3EVcW3jseHQ__&Key-Pair-Id=APKAJ4GOPJEICF5TREYA&Expires=1579984449"', {
+    //     method: "post",
     //     mode: "no-cors",
     //     headers: {
     //         'Authorization': '2e8d7cd2f48c9a0ab93d2c45a73013de'
     //     }
     // })
+    fetch("https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs/56356209", {
+        method: "get",
+        mode: "no-cors",
+        headers: {
+            'Authorization': '2e8d7cd2f48c9a0ab93d2c45a73013de'
+        }
+    })
       .then(res => res.json())
       .catch(console.log);
+  };
+
+  pauseMusic = async () => {
+    fetch('http://localhost:8080/pause', {
+      method: "post",
+      mode: "no-cors",
+      headers: {
+        'Authorization': '2e8d7cd2f48c9a0ab93d2c45a73013de'
+      }
+    })
+    .then(res => res.json())
+    .catch(console.log);
+  };
+
+  unpauseMusic = async () => {
+    fetch('http://localhost:8080/unpause', {
+      method: "post",
+      mode: "no-cors",
+      headers: {
+        'Authorization': '2e8d7cd2f48c9a0ab93d2c45a73013de'
+      }
+    })
+    .then(res => res.json())
+    .catch(console.log);
+  };
+
+  getPlayerStatus = async () => {
+    fetch('http://localhost:8080/status', {
+      method: "post",
+      mode: "no-cors",
+      headers: {
+        'Authorization': '2e8d7cd2f48c9a0ab93d2c45a73013de'
+      }
+    })
+    .then(console.log)
+    .catch(console.log);
   };
 
   search = async value => {
@@ -76,15 +112,18 @@ class App extends React.Component {
                     </List.Item>
                 )}
             />
-            <div style={{ background: '#fff', padding: 24, minHeight: 1000 }}>
+            <div style={{ background: '#fff', padding: 24, minHeight: '800' }}>
                 <Button type="primary" style={{ marginLeft: 8 }} onClick={this.handleChange}>
                     Primary Button
+                </Button>
+                <Button type="danger" style={{ marginLeft: 8 }} onClick={this.getPlayerStatus}>
+                    Player Status
                 </Button>
             </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
-          <Button type="primary" shape="circle" size="large" icon="pause" style={{backgroundColor: 'black'}}/>
-          <Button type="primary" shape="circle" size="large" icon="caret-right" style={{backgroundColor: 'black'}}/>
+          <Button type="primary" shape="circle" size="large" icon="pause" style={{backgroundColor: 'black'}} onClick={this.pauseMusic}/>
+          <Button type="primary" shape="circle" size="large" icon="caret-right" style={{backgroundColor: 'black'}} onClick={this.unpauseMusic}/>
           <Button type="primary" shape="circle" size="large" icon="forward" style={{backgroundColor: 'black'}}/>
           <p>ConUHacks V Project Submission by the Elite Four</p>
         </Footer>
